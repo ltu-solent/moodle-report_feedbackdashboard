f548c97d95840379f3f79d79d858b62083580bb3
Mark Sharp <mark.sharp@solent.ac.uk>
2025-05-15 10:41:14 +0100

    Remove deprecated legacy logging function

--- classes/event/report_viewed.php
+++ classes/event/report_viewed.php
@@ -70,17 +70,6 @@ class report_viewed extends \core\event\base {
     }
 
     /**
-     * Return the legacy event log data.
-     *
-     * @return array
-     */
-    protected function get_legacy_logdata() {
-        $url = "report/feedbackdashboard/user.php?id=". $this->userid . "&course=" . $this->courseid .
-            "&mode=" . $this->other['mode'];
-        return [$this->courseid, 'course', 'report feedbackdashboard', $url, $this->courseid];
-    }
-
-    /**
      * Returns relevant URL.
      *
      * @return \moodle_url
